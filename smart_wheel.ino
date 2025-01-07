c218c06858998432ef88c00be24d89d795dc02e0
tqkblx <il.mystafa@gmail.com>
2025-01-07 13:39:05 +0300

    Read and write touch sensor data using Arduino

--- smart_wheel.ino
+++ smart_wheel.ino
@@ -1,11 +1,21 @@
+#define TOUCH_SENSOR_INPUT_PIN 10
+
+#define TOUCH_SENSOR_OUTPUT_PIN 4
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(TOUCH_SENSOR_INPUT_PIN, INPUT);
+
+  pinMode(TOUCH_SENSOR_OUTPUT_PIN, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  int delay_ = 3000;
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(delay_);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(delay_);
+  int touch_sensor_output = digitalRead(TOUCH_SENSOR_INPUT_PIN);
+  Serial.print("Touch sensor output: ");
+  Serial.println(touch_sensor_output);
+  digitalWrite(TOUCH_SENSOR_OUTPUT_PIN, touch_sensor_output);
+  delay(1000);
 }
