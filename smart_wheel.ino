f039232b33da477cd9b842cd1f16babdd81e52cf
tqkblx <il.mystafa@gmail.com>
2025-01-15 15:47:18 +0300

    Change port numbers

--- smart_wheel.ino
+++ smart_wheel.ino
@@ -1,13 +1,9 @@
-#define TOUCH_SENSOR_INPUT_PIN 10
-
-#define TOUCH_SENSOR_OUTPUT_PIN 4
+#define TOUCH_SENSOR_INPUT_PIN 7
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(TOUCH_SENSOR_INPUT_PIN, INPUT);
-
-  pinMode(TOUCH_SENSOR_OUTPUT_PIN, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -16,6 +12,5 @@ void loop() {
   int touch_sensor_output = digitalRead(TOUCH_SENSOR_INPUT_PIN);
   Serial.print("Touch sensor output: ");
   Serial.println(touch_sensor_output);
-  digitalWrite(TOUCH_SENSOR_OUTPUT_PIN, touch_sensor_output);
   delay(1000);
 }
